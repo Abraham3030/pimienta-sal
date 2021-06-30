a21e45c0b1ff414bb83d5d7880de81027577b101
Abraham Montelongo <mocapunk@hotmail.com>
2021-06-29 22:07:18 -0500

    mricrodesafio 1 menu section dinamico con tags ejs <% %> <%= %>

--- controller/mainController.js
+++ controller/mainController.js
@@ -1,6 +1,34 @@
 const mainController = {
     index: (req,res) => {
-        res.render('index');
+        const meals = [
+        {
+            name: "Carpaccio fresco",
+            description: "Entrada Carpaccio de salmón con cítricos",
+            currency: "U$S",
+            price: 65.50
+        },
+        {
+            name: "Risotto de berenjena",
+            description: "Risotto de berenjena y queso de cabra",
+            currency: "U$S",
+            price: 47.00
+        },
+        {
+            name: "Mousse de arroz",
+            description: "Mousse de arroz con leche y aroma de azahar",
+            currency: "U$S",
+            price: 27.50
+        },
+        {
+            name: "Espárragos blancos",
+            description: "Espárragos blancos con vinagreta de verduras y jamón ibérico",
+            currency: "U$S",
+            price: 37.50
+        }
+
+
+        ]
+        res.render('index', {meals});
     },
     detalleMenu: (req,res) => {
         res.render('detalleMenu');
@@ -8,3 +36,4 @@ const mainController = {
 }
 
 module.exports = mainController;
+
